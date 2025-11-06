a0b3f04b462d6b2de234fd0ec2805e90b8b64951
yashikasharma0802-dotcom <yashikasharma0802@gmail.com>
2025-11-06 15:52:07 -0500

    Update wishlist.js

--- homepage/wishlist.js
+++ homepage/wishlist.js
@@ -6,3 +6,19 @@ wishlist.forEach(item => {
   li.textContent = item;
   listContainer.appendChild(li);
 });
+
+const buttons = document.querySelectorAll(".add-to-wishlist");
+
+buttons.forEach(button => {
+  button.addEventListener("click", function () {
+    let itemName = this.parentElement.querySelector("h3").innerText;
+
+    let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+    
+    if (!wishlist.includes(itemName)) {
+      wishlist.push(itemName);
+      localStorage.setItem("wishlist", JSON.stringify(wishlist));
+      alert(itemName + " added to wishlist!");
+    }
+  });
+});
