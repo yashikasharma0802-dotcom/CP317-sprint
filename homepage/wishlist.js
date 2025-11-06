ab25d48f8ce78c34999ca7352599074c97f02c32
yashikasharma0802-dotcom <yashikasharma0802@gmail.com>
2025-11-06 15:52:46 -0500

    Update wishlist.js

--- homepage/wishlist.js
+++ homepage/wishlist.js
@@ -22,3 +22,14 @@ buttons.forEach(button => {
     }
   });
 });
+
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+let listContainer = document.getElementById("wishlist-items");
+
+if (listContainer) {
+  wishlist.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item;
+    listContainer.appendChild(li);
+  });
+}
